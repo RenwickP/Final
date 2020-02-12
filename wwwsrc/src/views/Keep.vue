1868a37648941b3b95db577440918fd8a62aae3d
renwickp <peter2renwick@gmail.com>
2020-02-12 13:50:59 -0700

    Still working for some reason... :)

--- wwwsrc/src/views/Keep.vue
+++ wwwsrc/src/views/Keep.vue
@@ -2,6 +2,10 @@
   <div class="keep">
     <h1>Check out this keep</h1>
     <p>{{ keep.name }}</p>
+
+    <h2>Test Data</h2>
+    <div v-for="vault in vaults" :key="vault.id">{{ vault.id }}</div>
+    {{ vaults }}
   </div>
 </template>
 <script>
@@ -10,11 +14,16 @@ export default {
 
   mounted() {
     this.$store.dispatch("getKeepsById", this.$route.params.id);
+    this.$store.dispatch("getVaults");
   },
 
   computed: {
     keep() {
       return this.$store.state.activeKeep;
+    },
+
+    vaults() {
+      return this.$store.state.vaults;
     }
   }
 };
